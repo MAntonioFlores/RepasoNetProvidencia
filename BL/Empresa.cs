9479bcf988046c2beb5fb91cbf4ceb5504c5f553
MAntonioFlores <zerofenix.wrath@gmail.com>
2024-07-05 06:02:31 -0600

    Add project files.

---
+++ BL/Empresa.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Empresa
+    {
+        public static (bool, string, Exception) Add(ML.Empresa empresa)
+        {
+			try
+			{
+				using (DL.MFloresEmpresaEntities context = new DL.MFloresEmpresaEntities())
+				{
+					var rowsAffected = context.AddEmpresa(empresa.Nombre, empresa.Logo, empresa.URL, empresa.Direccion);
+
+					if (rowsAffected > 0 )
+					{
+						return (true, null, null);
+					}
+					else
+					{
+						return (false, null, null);
+					}
+
+				}
+			}
+			catch (Exception ex)
+			{
+
+				return (false, "Ocurrio un error: " + ex, ex);
+			}
+        }
+		public static (bool, string, Exception) Update(ML.Empresa empresa)
+		{
+			try
+			{
+				using (DL.MFloresEmpresaEntities context = new DL.MFloresEmpresaEntities())
+				{
+					var rowsAffected = context.UpdateEmpresa(empresa.IdEmpresa, empresa.Nombre, empresa.Logo, empresa.URL, empresa.Direccion);
+
+					if(rowsAffected > 0)
+					{
+						return (true, null, null);
+					}
+					else
+					{
+						return(false, null, null);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return(false, "Ocurrio un error: " + ex , ex);
+			}
+		}
+
+		public static(bool,string, Exception)Delete(int IdEmpresa)
+		{
+			try
+			{
+				using (DL.MFloresEmpresaEntities context = new DL.MFloresEmpresaEntities())
+				{
+					var rowsAffected = context.DeleteEmpresa(IdEmpresa);
+
+					if(rowsAffected > 0)
+					{
+						return (true, null, null);
+					}
+					else
+					{
+						return(false, null, null);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+                return (false, "Ocurrio un error: " + ex, ex);
+            }
+		}
+		public static(bool,string,ML.Empresa,Exception) GetAll()
+		{
+			ML.Empresa empresa = new ML.Empresa();
+			try
+			{
+				using (DL.MFloresEmpresaEntities context = new DL.MFloresEmpresaEntities())
+				{
+					var query = context.GetAllEmpresa().ToList();
+
+					if(query.Count > 0)
+					{
+						empresa.Empresas = new List<ML.Empresa>();
+						foreach (var registro in query)
+						{
+							ML.Empresa empresaObj = new ML.Empresa();
+
+							empresaObj.IdEmpresa = registro.IdEmpresa;
+							empresaObj.Nombre = registro.Nombre;
+							empresaObj.Logo = registro.Logo;
+							empresaObj.URL = registro.URL;
+							empresaObj.Direccion = registro.Dirección;
+
+							empresa.Empresas.Add(empresaObj);
+						}
+						return (true, null, empresa, null);
+					}
+					else
+					{
+						return (false, null,empresa, null);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+                return (false, "Ocurrio un error: " + ex, null, ex);
+            }
+		}
+		public static(bool,string, Exception)GetById(int? IdEmpresa)
+		{
+			ML.Empresa empresa = new ML.Empresa();
+			try
+			{
+				using (DL.MFloresEmpresaEntities context = new DL.MFloresEmpresaEntities())
+				{
+					var query = context.GetByIdEmpresa(IdEmpresa).SingleOrDefault();
+
+					if (query != null)
+					{
+						empresa.Nombre = query.Nombre;
+						empresa.Logo = query.Logo;
+						empresa.URL = query.URL;
+						empresa.Direccion = query.Dirección;
+
+						return (true, null, null);
+					}
+					else
+					{
+						return(false, null, null);
+					}
+
+				}
+			}
+			catch (Exception ex)
+			{
+                return (false, "Ocurrio un error: " + ex, ex);
+            }
+		}
+    }
+}
---
+++ PL/Controllers/EmpresaController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        public ActionResult GetAll()
+        {
+            ML.Empresa empresa = new ML.Empresa();
+
+            var result = BL.Empresa.GetAll();
+
+            if (result.Item1)
+            {
+                empresa = result.Item3;
+
+                return View(empresa);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Form(int? IdEmpresa)
+        {
+            ML.Empresa empresa = new ML.Empresa();
+
+            //Update
+            if (IdEmpresa != null)
+            {
+                
+                var result = BL.Empresa.GetById(IdEmpresa.Value);
+                if (result.Item1)
+                {
+                    return View(empresa);
+                }
+                else
+                {
+                    ViewBag.Text = "Ocurrio un problema" + result.Item2;
+                    return PartialView("Modal");
+                }
+
+            }
+
+            else
+            //Agregar
+            {
+                var result = BL.Empresa.Add(empresa);
+                if (result.Item1)
+                {
+                    return View(empresa);
+                }
+                else
+                {
+                    ViewBag.Text = "Ocurrió un error" + result.Item2;
+                    return PartialView("Modal");
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Empresa empresa)
+        {
+
+            if (empresa.IdEmpresa != 0)
+            {
+                var update = BL.Empresa.Update(empresa);
+                if (update.Item1)
+                {
+                    ViewBag.Text = "El Registro se ha Actualizado Exitosamente";
+                    return PartialView("Modal");
+
+                }
+                else
+                {
+                    ViewBag.Text = "Ocurrio un Error: El Registro No se Guardo Exitosamente";
+                    return PartialView("Modal");
+                }
+            }
+
+            else
+            {
+                var result = BL.Empresa.Add(empresa);
+                if (result.Item1)
+                {
+                    ViewBag.Text = "Se Agrego Exitosamente";
+                    return PartialView("Modal");
+                }
+                else
+                {
+                    ViewBag.Text = "No Se Agrego Exitosamente";
+                    return PartialView("Modal");
+                }
+
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int IdEmpresa)
+        {
+            if (IdEmpresa != 0)
+            {
+
+                var result = BL.Empresa.Delete(IdEmpresa);
+
+
+                ViewBag.Text = "Se Elimino Exitosamente";
+                return PartialView("Modal");
+
+                return RedirectToAction("GetAll");
+            }
+            else
+            {
+                ViewBag.Text = "No se Elimino Exitosamente";
+                return PartialView("Modal");
+
+                return RedirectToAction("GetAll");
+            }
+        }
+    }
+}
